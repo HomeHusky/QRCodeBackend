d1d75e4557be64b530c10a12eb56e75ba6fd2914
HomeHusky <vitrannhat1@gmail.com>
2024-01-05 09:18:50 +0700

    edit header

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const slugify = require('slugify');
 
 //middlewares
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', process.env.DOMAIN_CLIENT, process.env.DOMAIN_SERVER);
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     res.header('Access-Control-Allow-Credentials', true);
