7bf0f7ac2bfea834afde120d07f7f554d7a1c20e
HomeHusky <vitrannhat1@gmail.com>
2024-01-05 08:44:14 +0700

    edit cors

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use((req, res, next) => {
 app.use(express.json());
 app.use(
     cors({
-        origin: [process.env.DOMAIN_CLIENT, process.env.DOMAIN_CLIENT_IP],
+        origin: process.env.DOMAIN_CLIENT,
         // origin: '*',
         optionsSuccessStatus: 200
     })
