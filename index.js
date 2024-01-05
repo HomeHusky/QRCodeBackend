8c8877c2ed53b843be660c182dd0cd689e2ea5cd
HomeHusky <vitrannhat1@gmail.com>
2024-01-05 08:54:03 +0700

    edit header 2nd

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const networkInterfaces = os.networkInterfaces();
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', process.env.DOMAIN_CLIENT);
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
     res.header('Access-Control-Allow-Credentials', true);
     next();
 });
