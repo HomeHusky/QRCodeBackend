f53be8730f4273ccd64245bfdb499d67da2169e5
HomeHusky <vitrannhat1@gmail.com>
2024-01-06 13:39:32 +0700

    edit env

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const https = require("https");
+const fs = require("fs");
 const app = express();
 const cors = require('cors')
 const db = require("./models");
@@ -16,27 +18,37 @@ const slugify = require('slugify');
 
 //middlewares
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    // res.setHeader('Access-Control-Allow-Credentials', 'true');
-
+    res.header('Access-Control-Allow-Origin', '*'); // Thay bằng origin của bạn
+    res.header("Access-Control-Allow-Credentials", true);
     next();
 });
 
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
 
-// app.use(
-//     cors({
-//         origin: process.env.DOMAIN_CLIENT,
-//         credentials: true,
-//         // origin: '*',
-//         optionsSuccessStatus: 200
-//     })
-// );
+app.use(
+    cors({
+        origin: [process.env.DOMAIN_SERVER_HTTP_LOCAL
+            , process.env.DOMAIN_CLIENT_HTTP_LOCAL
+            , process.env.DOMAIN_SERVER_HTTPS_LOCAL
+            , process.env.DOMAIN_CLIENT_HTTPS_LOCAL
+            , process.env.DOMAIN_SERVER_HTTP_IP
+            , process.env.DOMAIN_CLIENT_HTTP_IP
+            , process.env.DOMAIN_SERVER_HTTPS_IP
+            , process.env.DOMAIN_CLIENT_HTTPS_IP
+            , process.env.DOMAIN_CLIENT_HTTPS_NAME
+        ],
+        credentials: true,
+        // origin: '*',
+        // optionsSuccessStatus: 200
+    })
+);
 
 app.use(cookieParser());
+app.use((req, res, next) => {
+    res.cookie('cookieName', 'cookieValue', { secure: true });
+    next();
+});
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -64,8 +76,15 @@ app.use("/api/comments", commentRoutes);
 app.use("/api/likes", likeRoutes);
 app.use("/api/relationships", relationshipRoutes);
 
+const sslOptions = {
+    key: fs.readFileSync('key/key.pem'),
+    cert: fs.readFileSync('key/cert.pem'),
+};
+
+const server = https.createServer(sslOptions, app);
+
 db.sequelize.sync().then(() => {
-    app.listen(process.env.PORT, () => {
+    server.listen(process.env.PORT, () => {
         console.log("Server run in " + process.env.PORT + "!");
     });
 });
